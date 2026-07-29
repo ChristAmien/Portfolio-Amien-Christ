2238e03dbfe54ee0b3f75603241be94ae3f2b8a0
Christ Amien <amienfabien@gmail.com>
2026-07-29 19:48:04 +0000

    app.jsx refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import { About } from './components/About'
 import { Technologies } from './components/Technologies'
 import { Experiences } from './components/Experiences'
 import { Projects } from './components/Projects'
-import { LANG } from './constants'
+
 
 function App() {
-  const [language, setLanguage] = useState('fr')
-  const lang = LANG[language]
 
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden bg-slate-950">
